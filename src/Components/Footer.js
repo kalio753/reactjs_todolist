02934a4c5b5b0aa62db62c992e651155816d210a
kalio753 <duongchikien2001@gmail.com>
2022-08-04 11:17:24 +0700

    Restructure v2

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,33 +8,35 @@ function Footer() {
 
     return (
         <>
-            <div className="footer">
-                <Progress
-                    percent={Math.floor(
-                        (context.tasks.reduce(
-                            (acc, task) => (task.isDone ? acc + 1 : acc),
-                            0
-                        ) *
-                            100) /
-                            context.tasks.length
-                    )}
-                    steps={context.tasks.length}
-                />
-                <div>
-                    Sort by: &nbsp;
-                    <Select
-                        value={context.sortType}
-                        style={{
-                            width: 120,
-                        }}
-                        onChange={(e) => context.handleSortChange(e)}
-                    >
-                        <Option value="default">Default</Option>
-                        <Option value="priority">Priority</Option>
-                        <Option value="progess">Done</Option>
-                    </Select>
+            {context.tasks?.length > 1 && !context.isSearched && (
+                <div className="footer">
+                    <Progress
+                        percent={Math.floor(
+                            (context.tasks.reduce(
+                                (acc, task) => (task.isDone ? acc + 1 : acc),
+                                0
+                            ) *
+                                100) /
+                                context.tasks.length
+                        )}
+                        steps={context.tasks.length}
+                    />
+                    <div>
+                        Sort by: &nbsp;
+                        <Select
+                            value={context.sortType}
+                            style={{
+                                width: 120,
+                            }}
+                            onChange={(e) => context.handleSortChange(e)}
+                        >
+                            <Option value="default">Default</Option>
+                            <Option value="priority">Priority</Option>
+                            <Option value="progess">Done</Option>
+                        </Select>
+                    </div>
                 </div>
-            </div>
+            )}
         </>
     )
 }
